c0905717d682e55941a4ecf52ad806a62ae01ed2
Michael Costa <macosta@inf.ufpel.edu.br>
2022-03-27 21:27:39 -0300

    fix name Seo component
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/Seo"
 import { Row, Col } from "react-flexbox-grid"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Profile" />
+    <Seo title="Profile" />
     <Row
       center="xs"
       middle="xs"
